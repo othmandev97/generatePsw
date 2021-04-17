11854e53894098d9242c791f3bb89e020e0fbee4
othmandev97 <othmanelkarkoubi9@gmail.com>
2021-04-17 21:50:00 +0000

    first-commit

--- generator/views.py
+++ generator/views.py
@@ -22,23 +22,3 @@ def home(request):
         PasswordGen += random.choice(characters)
 
     return render(request, 'generator/home.html',  {'password':PasswordGen} )
-
-
-# def password(request):
-#     pass
-    # characters = list('abcdefghijklmnopqrstuvwxyz')
-
-    # if request.GET.get('uppercase'):
-    #     characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
-    # if request.GET.get('specialchar'):
-    #     characters.extend(list('!@#$%^&*()'))
-    # if request.GET.get('numbers'):
-    #     characters.extend(list('0123456789'))
-
-    # length = int(request.GET.get('length',12))
-
-    # PasswordGen = ''
-    # for x in range(length):
-    #     PasswordGen += random.choice(characters)
-
-    # return render(request, 'generator/password.html', {'password':PasswordGen} )
